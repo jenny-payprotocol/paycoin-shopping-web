1ea3878bb34b9b7aa7f8fc6f60732a5da851bbee
jenny-payprotocol <105176539+jenny-payprotocol@users.noreply.github.com>
2024-11-27 15:30:20 +0900

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  weight: "45 920",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pretendard.variable} font-sans antialiased`}
       >
         {children}
       </body>
